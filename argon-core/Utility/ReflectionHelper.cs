b455a6065aea2072b3dfe0895e6a1d77265e442e
Jonny Coombes <jcoombes@jcs-software.co.uk>
2021-01-21 11:17:08 +0000

    Re-added in order to supplant later Neon Glow utilities

---
+++ argon-core/Utility/ReflectionHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JCS.Argon.Utility
+{
+    /// <summary>
+    /// Contains any reflection-related utilities
+    /// </summary>
+    public static class ReflectionHelper
+    {
+        /// <summary>
+        /// Searches the currently loaded assemblies for implementations of a given interface type
+        /// </summary>
+        /// <typeparam name="T">The type to be search for</typeparam>
+        /// <returns></returns>
+        public static IEnumerable<Type> LocateAllImplementors<T>()
+        {
+            var type = typeof(T);
+            var types = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(s => s.GetTypes())
+                .Where(p => type.IsAssignableFrom(p));
+            return types;
+        }
+
+        /// <summary>
+        /// Convenience method for the direct instantiation of a given type, with a supplied list of constructor
+        /// arguments
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="constructorParams">A variable list of constructor arguments</param>
+        /// <returns></returns>
+        public static object? InstantiateType(Type type, params object[] constructorParams)
+        {
+            return  Activator.CreateInstance(type, args:constructorParams); 
+        }
+    }
+}
