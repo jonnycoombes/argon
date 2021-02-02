1bf46cbc75f47f994cf3879a892d2f95dc18d7b5
Jonny Coombes <jcoombes@jcs-software.co.uk>
2021-02-02 11:10:28 +0000

    Tidying up of comments and file layout

--- argon-core/Services/ArgonApplicationBuilderExtension.cs
+++ argon-core/Services/ArgonApplicationBuilderExtension.cs
@@ -1,27 +1,43 @@
-﻿using System.Diagnostics;
+﻿#region
+
+using System.Diagnostics;
 using Microsoft.AspNetCore.Builder;
 using static System.DateTime;
+
+#endregion
 
 namespace JCS.Argon.Extensions
 {
     /// <summary>
-    /// Class which can be used in order to add middleware etc...
+    ///     Class which can be used in order to add middleware etc...
     /// </summary>
     public static class ArgonApplicationBuilderExtension
     {
+        /// <summary>
+        ///     Name for the Argon timings header - prefixed with X following convention for extended headers
+        /// </summary>
         public static string X_ARGON_TIMINGS_TOTAL = "X-Argon-Timing";
 
+        /// <summary>
+        ///     Name for the Argon timestamp header - prefixed with X following convention for extended headers
+        /// </summary>
         public static string X_ARGON_TIMESTAMP = "X-Argon-Timestamp";
 
+        /// <summary>
+        ///     Extension which just injects timings code before and after a given request, which can then be injected into the
+        ///     response headers
+        /// </summary>
+        /// <param name="app">The current <see cref="IApplicationBuilder" /></param>
+        /// <returns></returns>
         public static IApplicationBuilder UseArgonTelemetry(this IApplicationBuilder app)
         {
             app.Use(async (context, next) =>
             {
                 var timer = new Stopwatch();
                 timer.Start();
-#pragma warning disable 1998
+                #pragma warning disable 1998
                 context.Response.OnStarting(async () =>
-#pragma warning restore 1998
+                    #pragma warning restore 1998
                 {
                     timer.Stop();
                     context.Response.Headers.Add(X_ARGON_TIMINGS_TOTAL, timer.ElapsedMilliseconds.ToString());
