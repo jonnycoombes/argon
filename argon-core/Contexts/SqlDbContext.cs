29352652c1ae196122cfe335f0f2a8d220bf1cb4
Jonny Coombes <jcoombes@jcs-software.co.uk>
2021-02-02 11:08:07 +0000

    Tidying up of comments and file layout

--- argon-core/Contexts/SqlDbContext.cs
+++ argon-core/Contexts/SqlDbContext.cs
@@ -1,62 +1,71 @@
+#region
+
 using System;
 using System.Diagnostics.CodeAnalysis;
 using JCS.Argon.Model.Schema;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 
+#endregion
+
 namespace JCS.Argon.Contexts
 {
     /// <summary>
-    /// The main database context used throughout the core
+    ///     The main database context used throughout the core
     /// </summary>
     public class SqlDbContext : DbContext
     {
-        public SqlDbContext([NotNullAttribute] DbContextOptions options) : base(options)
+        /// <summary>
+        ///     Default constructor
+        /// </summary>
+        /// <param name="options"></param>
+        public SqlDbContext([NotNullAttribute]
+            DbContextOptions options) : base(options)
         {
         }
 
         /// <summary>
-        /// The set of all <see cref="Collection"/> model elements
+        ///     The set of all <see cref="Collection" /> model elements
         /// </summary>
         public DbSet<Collection> Collections { get; set; } = null!;
 
         /// <summary>
-        /// The set of all <see cref="Item"/> model elements
+        ///     The set of all <see cref="Item" /> model elements
         /// </summary>
         public DbSet<Item> Items { get; set; } = null!;
 
         /// <summary>
-        /// The set of all <see cref="ItemVersion"/> model elements
+        ///     The set of all <see cref="ItemVersion" /> model elements
         /// </summary>
         public DbSet<ItemVersion> Versions { get; set; } = null!;
 
         /// <summary>
-        /// The set of all <see cref="PropertyGroup"/> model elements
+        ///     The set of all <see cref="PropertyGroup" /> model elements
         /// </summary>
         public DbSet<PropertyGroup> PropertyGroups { get; set; } = null!;
 
         /// <summary>
-        /// The set of all <see cref="Property"/> model elements
+        ///     The set of all <see cref="Property" /> model elements
         /// </summary>
         public DbSet<Property> Properties { get; set; } = null!;
 
         /// <summary>
-        /// The set of all <see cref="ConstraintGroup"/> model elements
+        ///     The set of all <see cref="ConstraintGroup" /> model elements
         /// </summary>
         public DbSet<ConstraintGroup> ConstraintGroups { get; set; } = null!;
 
         /// <summary>
-        /// The set of all <see cref="Constraint"/> model elements
+        ///     The set of all <see cref="Constraint" /> model elements
         /// </summary>
         public DbSet<Constraint> Constraints { get; set; } = null!;
 
         /// <summary>
-        /// The set of all <see cref="CacheEntry"/> model elements
+        ///     The set of all <see cref="CacheEntry" /> model elements
         /// </summary>
         public DbSet<CacheEntry> CacheEntries { get; set; } = null!;
 
         /// <summary>
-        /// Perform any more technical modifications to the schema creation logic in here
+        ///     Perform any more technical modifications to the schema creation logic in here
         /// </summary>
         /// <param name="modelBuilder"></param>
         protected override void OnModelCreating(ModelBuilder modelBuilder)
--- argon-core/Contexts/SqlDbManager.cs
+++ argon-core/Contexts/SqlDbManager.cs
@@ -1,12 +1,16 @@
+#region
+
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
+#endregion
+
 namespace JCS.Argon.Contexts
 {
     /// <summary>
-    /// This class can be used to perform any startup/initialisation logic against the database
-    /// at application startup
+    ///     This class can be used to perform any startup/initialisation logic against the database
+    ///     at application startup
     /// </summary>
     public class SqlDbManager
     {
