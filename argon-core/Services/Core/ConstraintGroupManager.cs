2b544d2ce64ec6d33158e5d601c69d4d5fe75983
Jonny Coombes <jcoombes@jcs-software.co.uk>
2021-02-03 06:57:59 +0000

    Consistency variable name change

--- argon-core/Services/Core/ConstraintGroupManager.cs
+++ argon-core/Services/Core/ConstraintGroupManager.cs
@@ -96,27 +96,24 @@ namespace JCS.Argon.Services.Core
                         if (cmd.TargetProperty == null)
                             throw new IConstraintGroupManager.ConstraintGroupManagerException(StatusCodes.Status400BadRequest,
                                 $"No target property specified for constraint [{cmd.Name}]");
-
                         break;
                     case ConstraintType.AllowableType:
                         if (cmd.ValueType == null)
                             throw new IConstraintGroupManager.ConstraintGroupManagerException(StatusCodes.Status400BadRequest,
                                 $"No value type specified for constraint [{cmd.Name}]");
-
                         break;
                     case ConstraintType.AllowableTypeAndValues:
                         if (cmd.ValueType == null || cmd.AllowableValues == null)
                             throw new IConstraintGroupManager.ConstraintGroupManagerException(StatusCodes.Status400BadRequest,
                                 $"No value type or value set specified for constraint [{cmd.Name}]");
-
                         break;
                     default:
                         throw new ArgumentOutOfRangeException();
                 }
 
-                var awaiter = await DbContext.AddAsync(constraint);
+                var addOp = await DbContext.AddAsync(constraint);
                 await DbContext.SaveChangesAsync();
-                return awaiter.Entity;
+                return addOp.Entity;
             }
             catch (Exception ex)
             {
