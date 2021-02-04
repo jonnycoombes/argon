478d5594c2f458c39f46b9c3be9c0edc360e18b1
Jonny Coombes <jcoombes@jcs-software.co.uk>
2021-02-04 15:02:37 +0000

    Removed - no longer needed, have reverted to utilising LINQ aggregate/fold operation instead

--- argon-core/Utility/StringHelper.cs
+++ argon-core/Utility/StringHelper.cs
@@ -10,11 +10,15 @@ namespace JCS.Argon.Utility
 {
     public static class StringHelper
     {
+        /// <summary>
+        ///     Simple utility for concatenating a list of strings - should be replaced by LINQ aggregate function
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
         public static string CollapseStringList(IEnumerable<string> source)
         {
             var sb = new StringBuilder();
             foreach (var s in source) sb.Append($"{s}{Environment.NewLine}");
-
             return sb.ToString();
         }
     }
