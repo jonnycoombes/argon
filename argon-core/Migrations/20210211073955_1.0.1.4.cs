35ad6bab72dc3e0f00985ce958bb3cc7e41e8a9d
Jonny Coombes <jcoombes@jcs-software.co.uk>
2021-02-11 07:42:13 +0000

    Migration to rename core collection columns

---
+++ argon-core/Migrations/20210211073955_1.0.1.4.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JCS.Argon.Migrations
+{
+    public partial class _1014 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                "Size",
+                schema: "argon",
+                table: "collection",
+                newName: "TotalSizeBytes");
+
+            migrationBuilder.RenameColumn(
+                "Length",
+                schema: "argon",
+                table: "collection",
+                newName: "NumberOfItems");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                "TotalSizeBytes",
+                schema: "argon",
+                table: "collection",
+                newName: "Size");
+
+            migrationBuilder.RenameColumn(
+                "NumberOfItems",
+                schema: "argon",
+                table: "collection",
+                newName: "Length");
+        }
+    }
+}
