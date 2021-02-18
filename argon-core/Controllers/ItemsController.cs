34379b4991a2bc0a3ad942709ef147fc7416799c
Jonny Coombes <jcoombes@jcs-software.co.uk>
2021-02-18 07:48:09 +0000

    Added documentation for 400 response code

--- argon-core/Controllers/ItemsController.cs
+++ argon-core/Controllers/ItemsController.cs
@@ -266,6 +266,7 @@ namespace JCS.Argon.Controllers
         /// <returns></returns>
         /// <response code="200">Successful update</response>
         /// <response code="404">Invalid item id provided, or no properties present in the inbound request</response>
+        /// <response code="400">Bad request - this will be returned if the constraints relating to the parent collection are violated</response>
         /// <response code="500">Internal server error</response>
         [HttpPatch]
         [Route("/api/v1/Collections/{collectionId}/Items/{itemId}")]
